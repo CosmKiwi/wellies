91d3ba556e7cffdbb96b6dbf5b55321427235677
Cosm Kiwi <andymc47@hotmail.com>
2026-04-22 23:13:28 +1200

    correcting favicon

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href="images/gumboots_460.png" />
+</svelte:head>
+
 {@render children()}
